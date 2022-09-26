5735771af6889d5ac209ee75f9ef6bdaffc4bca7
jwrr <jwrr.com@gmail.com>
2022-09-25 23:12:01 -0400

    textbox - support resizing of windows based on getmaxyx

--- textbox_lcurses/filebrowser.lua
+++ textbox_lcurses/filebrowser.lua
@@ -24,6 +24,21 @@ local stringx = require"pl.stringx"
 local textbox = require"textbox"
 local dirtree = require"dirtree"
 
+
+function resize_windows(stdscr)
+  local maxy, maxx = stdscr:getmaxyx()
+  local prev_maxy, prev_maxx = textbox.getmaxyx()
+  if  maxy ~= prev_maxy or maxx ~= prev_maxx then
+    textbox.setmaxyx(maxy, maxx)
+    textbox.update({name = "banner", width = maxx})
+    textbox.update({name = "editor", width = maxx - 36, height = maxy - 11})
+    textbox.update({name = "status", width = maxx - 36, starty = maxy - 10})
+    textbox.update({name = "nav",    height = maxy-1})
+  end
+  return maxy, maxx
+end
+
+
 function filebrowser()
   local stdscr = curses.initscr()
 
@@ -33,31 +48,60 @@ function filebrowser()
   curses.nl(true)
   curses.keypad()
 
-  textbox.init_colors()
+  textbox.start_color()
 
-  textbox.new({name = 'banner',  height =  1, width = 137, starty =  0, startx =  0, hasborder = false, txtcolor = textbox.black_on_white, rpad = true })
-  textbox.new({name = 'nav',     height = 40, width =  35, starty =  1, startx =  0, hasborder = true,  txtcolor = textbox.black_on_white, rpad  = true})
-  textbox.new({name = 'status',  height = 10, width = 100, starty = 31, startx = 36, hasborder = true,  txtcolor = textbox.red_on_black })
-  textbox.new({name = 'editor',  height = 30, width = 100, starty =  1, startx = 36, hasborder = true,  txtcolor = textbox.white_on_black, active = true })
+  textbox.new({name = 'banner',  height =  1, width = 137, starty =  0, startx =  0, hasborder = false, color_pair = textbox.black_on_white})
+  textbox.new({name = 'nav',     height = 40, width =  35, starty =  1, startx =  0, hasborder = true,  color_pair = textbox.black_on_white})
+  textbox.new({name = 'status',  height = 10, width = 100, starty = 31, startx = 35, hasborder = true,  color_pair = textbox.red_on_black })
+  textbox.new({name = 'editor',  height = 30, width = 100, starty =  1, startx = 35, hasborder = true,  color_pair = textbox.white_on_black, active = true })
 
-  local banner = "Enter Ctrl-Q to quit"
   local txt = ''
+  local esc_string = ''
 
+  local c = stdscr:getch()
+  local banner ='Enter Ctrl-Q to quit'
+  local prev_maxy, prev_maxx = stdscr:getmaxyx()
   repeat
     local file_str = dirtree.str("..")
     local file_tab = dirtree.lines("..")
 
     textbox.print('banner', banner)
     textbox.print_table('nav', file_tab)
-    textbox.print('status', txt)
-    textbox.print('editor', txt)
+    if esc_string == "" then
+      textbox.print('status', txt)
+      textbox.print('editor', txt)
+    end
 
     local c = stdscr:getch()
+
     local is_quit_key  = (c == 17)
     local is_valid_key = (c <= 255)
     local is_enter_key = (c == 10)
     local is_backspace_key  = (c == 8) or (c == 127)
     local ch = is_valid_key and string.char(c) or ''
+
+      if is_backspace_key then
+        txt = txt:sub(1, -2)
+      else
+        txt = txt .. ch
+      end
+
+    if c == 27 then
+        esc_string = 'esc' -- to_string(27)
+    elseif esc_string == '' then
+--       if is_backspace_key then
+--         txt = txt:sub(1, -2)
+--       else
+--         txt = txt .. ch
+--       end
+    elseif is_command(esc_string) then
+      esc_string = ''
+    elseif is_enter_key then
+      esc_string = ''
+    else
+      esc_string = esc_string ..  string.char(c)
+    end
+
 
     local ch_banner = ch
     if is_enter_key then
@@ -66,19 +110,10 @@ function filebrowser()
       ch_banner = '<bs>'
     end
 
-    local maxy, maxx = stdscr:getmaxyx()
-
--- --     if  maxy /= prev_maxy or maxx /= prev_maxy then
---
---     end
-    banner = "Enter Ctrl-Q to quit, '" .. ch_banner  .. "' (" .. tostring(c)  ..  '), size= ' .. tostring(maxx) .. 'x' .. tostring(maxy)
-
-    if is_backspace_key then
-      txt = txt:sub(1, -2)
-    else
-      txt = txt .. ch
-    end
-
+    local maxy, maxx = resize_windows(stdscr)
+    banner = "Enter Ctrl-Q to quit, '" .. ch_banner  .. "' (" .. tostring(c)  ..  '), size= ' .. tostring(maxx) .. 'x' .. tostring(maxy) .. esc_string
+--       1         2         3         4         5         6                   8                   0                   2                   4
+-- 4567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890
   until is_quit_key
 
   curses.endwin()
--- textbox_lcurses/textbox.lua
+++ textbox_lcurses/textbox.lua
@@ -29,6 +29,17 @@
   local dirtree = require"dirtree"
 
 
+  function M.setmaxyx(maxy, maxx)
+    M.maxy = maxy
+    M.maxx = maxx
+  end
+
+  
+  function M.getmaxyx()
+    return M.maxy, M.maxx
+  end
+
+
   M.all_windows = {}
   M.active_window = ""
 
@@ -49,12 +60,12 @@
   end
 
 
-  function M.color(name, color)
-    M.all_windows[name].win:attron(curses.color_pair(color))
+  function M.set_color_pair(name, color_pair)
+    M.all_windows[name].win:attron(curses.color_pair(color_pair))
   end
 
 
-  function M.init_colors()
+  function M.start_color()
     M.black_on_black   = 1
     M.red_on_black     = 2
     M.green_on_black   = 3
@@ -103,24 +114,80 @@
   end
 
 
-  function M.tmerge(t1, t2)
+  function M.shallow_merge(t1, t2)
     for k,v in pairs(t2) do
       t1[k] = v
     end
   end
 
-  function M.update(name, cfg)
-    local this = M.all_windows[name]
-    M.tmerge(this, cfg)
-  end
-
 
   function M.newbox(cfg)
     local this = {}
-    M.tmerge(this, cfg)
+    M.shallow_merge(this, cfg)
     this.isbox = true
     this.win = curses.newwin(this.height, this.width, this.starty, this.startx)
     return this
+  end
+
+
+  function M.update(cfg)
+    cfg.name   = cfg.name or "error-missing-name"
+    local this = M.all_windows[cfg.name] or {}
+    cfg.hasborder = cfg.hasborder or this.hasborder
+    cfg.height = cfg.height or this.height
+    cfg.width  = cfg.width or this.width
+    cfg.starty = cfg.starty or this.starty
+    cfg.startx = cfg.startx or this.startx
+    local name = cfg.name
+
+    if cfg.hasborder then
+      local boxname = name .. "_box"
+      thisbox = M.all_windows[boxname]
+      if cfg.height ~= thisbox.height or cfg.width ~= thisbox.width then
+        thisbox.win:resize(cfg.height, cfg.width)
+        thisbox.win:clear()
+        thisbox.win:box(0,0)
+      end
+      if cfg.starty ~= this.starty or cfg.startx ~= thisbox.x then
+        thisbox.win:move_window(cfg.starty, cfg.startx)
+        thisbox.win:clear()
+        thisbox.win:box(0,0)
+      end
+
+      cfg.txt_height = cfg.height - 2
+      cfg.txt_width  = cfg.width - 2
+      if cfg.txt_height < 1 then cfg.txt_height = 1 end
+      if cfg.txt_width < 1 then cfg.txt_width = 1 end
+      cfg.txt_starty = cfg.starty + 1
+      cfg.txt_startx = cfg.startx + 1
+    else
+      cfg.txt_height = cfg.txt_height or this.txt_height
+      cfg.txt_width  = cfg.txt_width  or this.txt_width
+      cfg.txt_starty = cfg.txt_starty or this.txt_starty
+      cfg.txt_startx = cfg.txt_startx or this.txt_startx
+    end
+
+
+    if cfg.txt_height ~= this.txt_height or cfg.txt_width ~= this.txt_width then
+      this.win:resize(cfg.txt_height, cfg.txt_width)
+      this.win:clear()
+    end
+    if cfg.txt_starty ~= this.txt_starty or cfg.txt_startx ~= this.txt_startx then
+      this.win:move_window(cfg.txt_starty, cfg.txt_startx)
+      this.win:clear()
+    end
+
+    M.shallow_merge(this, cfg)
+
+    if this.active then
+      M.active_window = "name"
+    end
+    if this.color_pair then
+      M.set_color_pair(name, this.color_pair)
+    end
+    
+    this.win:wbkgd(curses.color_pair(this.color_pair))
+
   end
 
 
@@ -150,16 +217,19 @@
     end
 
     local this = {}
-    M.tmerge(this, cfg)
+    M.shallow_merge(this, cfg)
     this.isbox = false
     this.win   = curses.newwin(cfg.txt_height, cfg.txt_width, cfg.txt_starty, cfg.txt_startx)
     M.all_windows[name] = this
+    M.all_windows[name].id = #M.all_windows[name]
     if this.active then
       M.active_window = "name"
     end
-    if this.txtcolor then
-      M.color(name, this.txtcolor)
+    if this.color_pair then
+      M.set_color_pair(name, this.color_pair)
     end
+    
+    this.win:wbkgd(curses.color_pair(this.color_pair))
 
     if this.filename then
       this.lines = utils.readlines(this.filename)
@@ -174,7 +244,7 @@
       local box_name = name .. '_box'
       local color = M.white_on_black
       if this.active then
-        M.color(box_name, M.yellow_on_black)
+        M.set_color_pair(box_name, M.yellow_on_black)
       end
       M.all_windows[box_name].win:box(0, 0)
       M.all_windows[box_name].win:refresh()
@@ -182,21 +252,23 @@
     this.win:refresh()
   end
 
+  
+  function M.refresh_all()
+    for k,v in ipairs(M.all_windows) do
+      M.refresh(k)
+    end
+  end
+
 
   function M.print_table(name, t, action)
     action = action or 'init' -- 'init', 'insert'
     local this = M.all_windows[name]
     local txt_width = this.txt_width - 1
-
     this.win:move(0,0)
     for i,v in ipairs(t) do
       local line = stringx.rstrip(v, "\n\r")
       local endl = (line ~= v)
       line = stringx.shorten(line, txt_width)
-
-      if this.rpad then
-        line = M.rpad(line, txt_width)
-      end
       local lastline = (i == #t)
       if endl or not lastline then
         line = line .. "\n"
